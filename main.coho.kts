dcbab0736775747542715c363d5ade80e4175723
Fish <me@fishies.dev>
2025-11-10 17:47:20 -0800

    add templated sponsor

--- main.coho.kts
+++ main.coho.kts
@@ -1,11 +1,29 @@
+data class Sponsor(val name: String, val url: String, val image: String) {
+    fun toMap() = mapOf(
+        "name" to name,
+        "url" to url,
+        "image" to image,
+    )
+}
+
+
 root {
+    val sponsors = listOf(
+        Sponsor("NASA", "https://www.nasa.gov/", "images/nasasponsor1.png"),
+        Sponsor("Dunn Lumber", "#", "images/dunnlumbersponsor.png"),
+    )
+
+    KtHtmlFile.globalContext = mapOf(
+        "sponsors" to sponsors.map { it.toMap() },
+    )
+
     html(src("calendar.html"))
     html(src("calendar.css"))
     html(src("contact.html"))
     html(src("contact.css"))
     html(src("events.html"))
     html(src("events.css"))
-    html(src("index.html"))
+    ktHtml(src("index.html"))
     html(src("resources.html"))
     html(src("resources.css"))
     html(src("team.html"))
